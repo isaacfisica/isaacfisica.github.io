97bb560fed4860b2163979cb5b34a1fd80b2e2a9
isaac.fisica <isaac.fisica.kr@gmail.com>
2026-06-20 14:48:02 +0200

    build error to be fixed

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,18 @@ const basePath = process.env.PAGES_BASE_PATH || '';
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  images: { unoptimized: true },
-  basePath,
-  assetPrefix: basePath || undefined,
-  env: { NEXT_PUBLIC_BASE_PATH: basePath },
+  images: { 
+    unoptimized: true,
+    // images.domains → images.remotePatterns 로 변경 권장
+    remotePatterns: []
+  },
+  ...(basePath && { 
+    basePath, 
+    assetPrefix: basePath 
+  }),
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 };
 
 export default nextConfig;
